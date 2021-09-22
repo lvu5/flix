b9ca51d8e8fffa4351b128821307bb0a37d36be2
lvu5 <lvu5@uic.edu>
2021-09-22 11:14:00 +0700

    update part 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Linh T. Vu on 9/22/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+    
+    
+}
